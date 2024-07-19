041b94e042cf8c5340007c74f0abbce5612db49f
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2024-07-19 14:06:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,3 @@
-resource "spacelift_stack_dependency" "this" {
-  stack_id            = spacelift_stack.first_stack.id
-  depends_on_stack_id = spacelift_stack.second_stack.id
-}
-
-resource "spacelift_stack_dependency_reference" "this" {
-  stack_dependency_id = spacelift_stack_dependency.this.id
-  output_name         = "third_stack_output"
-  input_name          = "TF_VAR_third_stack_output"
-}
-
-resource "spacelift_stack_dependency" "that" {
-  stack_id            = spacelift_stack.first_stack.id
-  depends_on_stack_id = spacelift_stack.third_stack.id
-}
-
-resource "spacelift_stack_dependency_reference" "that" {
-  stack_dependency_id = spacelift_stack_dependency.that.id
-  output_name         = "second_stack_output"
-  input_name          = "TF_VAR_second_stack_output"
-}
 
 resource "spacelift_stack" "first_stack" {
   branch     = "main"
